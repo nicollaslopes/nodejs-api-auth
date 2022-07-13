3479071f189311190ce22e8030b735d1df7976aa
nicollaslopes <nicollaslopesn@gmail.com>
2022-07-13 20:41:42 -0300

    criando conexão com o bd

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const dotenv = require('dotenv').config()
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000, () => console.log('App rodando na porta 3000'));
+const USER_DB = process.env.USER_DB
+const PASS_DB = process.env.PASS_DB
+
+mongoose.connect(`mongodb+srv://${USER_DB}:${PASS_DB}@cluster0.91afl.mongodb.net/?retryWrites=true&w=majority`, () => console.log('Conexão com o db feita com sucesso!'))
+
+const authRoute = require('./routes/auth');
+
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('app rodando na porta 3000'));
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/registrar', (req, res) => {
+    res.send('registrar');
+});
+
+// router.post('/login', (req, res) => {
+
+// });
+
+module.exports = router;
