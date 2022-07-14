ab5b2d8bb45ab5ef629bfca129adff334fb0bd9a
nicollaslopes <nicollaslopesn@gmail.com>
2022-07-13 21:26:11 -0300

    criação da model

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const authRoute = require('./routes/auth');
+
 const USER_DB = process.env.USER_DB
 const PASS_DB = process.env.PASS_DB
 
-mongoose.connect(`mongodb+srv://${USER_DB}:${PASS_DB}@cluster0.91afl.mongodb.net/?retryWrites=true&w=majority`, () => console.log('Conexão com o db feita com sucesso!'))
+mongoose.connect(`mongodb+srv://${USER_DB}:${PASS_DB}@cluster0.91afl.mongodb.net/?retryWrites=true&w=majority`, () => console.log('connected to db!'))
 
-const authRoute = require('./routes/auth');
+app.use(express.json());
 
 app.use('/api/user', authRoute);
 
-app.listen(3000, () => console.log('app rodando na porta 3000'));
+app.listen(3000, () => console.log('app is running on port 3000'));
