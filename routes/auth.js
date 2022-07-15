9b31378e477150692a567c2c7be355a36c496d08
nicollaslopes <nicollaslopesn@gmail.com>
2022-07-14 21:51:03 -0300

    tratando erros

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 const User = require('../model/User');
 
 router.post('/register', async (req, res) => {
+
+    const { error } = schema.validate(req.body)
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
