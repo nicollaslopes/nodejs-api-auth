e0e221d9dba5a67b04cf066dab86be70ec071a5d
nicollaslopes <nicollaslopesn@gmail.com>
2022-07-14 22:00:33 -0300

    refatorando a validacao do registro

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-});
+const { registerValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
 
-    const { error } = schema.validate(req.body)
+    const { error } = registerValidation(req.body)
 
     if (error) {
         return res.status(400).send(error.details[0].message);
