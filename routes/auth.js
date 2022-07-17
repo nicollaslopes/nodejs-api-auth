840d6ceb6f33f240008976a904e4d50ad8937988
nicollaslopes <nicollaslopesn@gmail.com>
2022-07-17 10:40:52 -0300

    gerando jwt na autenticacao

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation')
+const { registerValidation, loginValidation } = require('../validation')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', async (req, res) => {
 
@@ -32,6 +33,34 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         res.status(400).send(err);
     }
+
+});
+
+router.post('/login', async (req, res) => {
+
+    const { error } = loginValidation(req.body)
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const user = await User.findOne({email: req.body.email})
+
+    if (!user) {
+        return res.status(400).send('E-mail is not found');
+    }
+
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+
+    if (!validPass) {
+        return res.status(400).send('Invalid password!');
+    }
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token);
+
+
+    
 
 });
 
